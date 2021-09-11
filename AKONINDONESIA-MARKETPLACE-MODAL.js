0658e46b3e1ec474a779ab4a8a5310b4068678ac
akonindonesia <90517402+akonindonesia@users.noreply.github.com>
2021-09-12 01:08:26 +0700

    Add files via upload

---
+++ AKONINDONESIA-MARKETPLACE-MODAL.js
@@ -0,0 +1,315 @@
+var modal1 = document.getElementById("fashion1");
+var btn = document.getElementById("fashionpria");
+var span = document.getElementsByClassName("Modalfashion1close")[0];
+btn.onclick = function () {
+    modal1.style.display = "block";
+}
+span.onclick = function () {
+    modal1.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal1.style.display = "none";
+    }
+}
+
+var modal2 = document.getElementById("fashion2");
+var btn = document.getElementById("fashionwanita");
+var span = document.getElementsByClassName("Modalfashion2close")[0];
+btn.onclick = function () {
+    modal2.style.display = "block";
+}
+span.onclick = function () {
+    modal2.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal2.style.display = "none";
+    }
+}
+
+var modal3 = document.getElementById("fashion3");
+var btn = document.getElementById("fashionbayianak");
+var span = document.getElementsByClassName("Modalfashion3close")[0];
+btn.onclick = function () {
+    modal3.style.display = "block";
+}
+span.onclick = function () {
+    modal3.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal3.style.display = "none";
+    }
+}
+
+var modal4 = document.getElementById("fashion4");
+var btn = document.getElementById("fashionmuslim");
+var span = document.getElementsByClassName("Modalfashion4close")[0];
+btn.onclick = function () {
+    modal4.style.display = "block";
+}
+span.onclick = function () {
+    modal4.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal4.style.display = "none";
+    }
+}
+
+var modal5 = document.getElementById("fashion5");
+var btn = document.getElementById("tas");
+var span = document.getElementsByClassName("Modalfashion5close")[0];
+btn.onclick = function () {
+    modal5.style.display = "block";
+}
+span.onclick = function () {
+    modal5.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal5.style.display = "none";
+    }
+}
+
+var modal6 = document.getElementById("fashion6");
+var btn = document.getElementById("aksesoris");
+var span = document.getElementsByClassName("Modalfashion6close")[0];
+btn.onclick = function () {
+    modal6.style.display = "block";
+}
+span.onclick = function () {
+    modal6.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal6.style.display = "none";
+    }
+}
+
+var modal7 = document.getElementById("fashion7");
+var btn = document.getElementById("topi");
+var span = document.getElementsByClassName("Modalfashion7close")[0];
+btn.onclick = function () {
+    modal7.style.display = "block";
+}
+span.onclick = function () {
+    modal7.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal7.style.display = "none";
+    }
+}
+
+var modal8 = document.getElementById("fashion8");
+var btn = document.getElementById("sepatu");
+var span = document.getElementsByClassName("Modalfashion8close")[0];
+btn.onclick = function () {
+    modal8.style.display = "block";
+}
+span.onclick = function () {
+    modal8.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal8.style.display = "none";
+    }
+}
+
+var modal9 = document.getElementById("fashion9");
+var btn = document.getElementById("sepatupria");
+var span = document.getElementsByClassName("Modalfashion9close")[0];
+btn.onclick = function () {
+    modal9.style.display = "block";
+}
+span.onclick = function () {
+    modal9.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal9.style.display = "none";
+    }
+}
+
+var modal10 = document.getElementById("fashion10");
+var btn = document.getElementById("sepatuwanita");
+var span = document.getElementsByClassName("Modalfashion10close")[0];
+btn.onclick = function () {
+    modal10.style.display = "block";
+}
+span.onclick = function () {
+    modal10.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal10.style.display = "none";
+    }
+}
+
+var modal11 = document.getElementById("elektroniks");
+var btn = document.getElementById("elektronik");
+var span = document.getElementsByClassName("Modalfashion11close")[0];
+btn.onclick = function () {
+    modal11.style.display = "block";
+}
+span.onclick = function () {
+    modal11.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal11.style.display = "none";
+    }
+}
+
+var modal12 = document.getElementById("gadgets");
+var btn = document.getElementById("gadget");
+var span = document.getElementsByClassName("Modalfashion12close")[0];
+btn.onclick = function () {
+    modal12.style.display = "block";
+}
+span.onclick = function () {
+    modal12.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal12.style.display = "none";
+    }
+}
+
+var modal13 = document.getElementById("komputers");
+var btn = document.getElementById("komputer");
+var span = document.getElementsByClassName("Modalfashion13close")[0];
+btn.onclick = function () {
+    modal13.style.display = "block";
+}
+span.onclick = function () {
+    modal13.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal13.style.display = "none";
+    }
+}
+
+var modal14 = document.getElementById("sepedas");
+var btn = document.getElementById("sepeda");
+var span = document.getElementsByClassName("Modalfashion14close")[0];
+btn.onclick = function () {
+    modal14.style.display = "block";
+}
+span.onclick = function () {
+    modal14.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal14.style.display = "none";
+    }
+}
+
+var modal15 = document.getElementById("otomotifs");
+var btn = document.getElementById("otomotif");
+var span = document.getElementsByClassName("Modalfashion15close")[0];
+btn.onclick = function () {
+    modal15.style.display = "block";
+}
+span.onclick = function () {
+    modal15.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal15.style.display = "none";
+    }
+}
+
+var modal16 = document.getElementById("mainans");
+var btn = document.getElementById("mainan");
+var span = document.getElementsByClassName("Modalfashion16close")[0];
+btn.onclick = function () {
+    modal16.style.display = "block";
+}
+span.onclick = function () {
+    modal16.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal16.style.display = "none";
+    }
+}
+
+var modal17 = document.getElementById("sembakos");
+var btn = document.getElementById("sembako");
+var span = document.getElementsByClassName("Modalfashion17close")[0];
+btn.onclick = function () {
+    modal17.style.display = "block";
+}
+span.onclick = function () {
+    modal17.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal17.style.display = "none";
+    }
+}
+
+var modal17 = document.getElementById("sembakos");
+var btn = document.getElementById("sembako");
+var span = document.getElementsByClassName("Modalfashion17close")[0];
+btn.onclick = function () {
+    modal17.style.display = "block";
+}
+span.onclick = function () {
+    modal17.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal17.style.display = "none";
+    }
+}
+
+var modal17 = document.getElementById("sembakos");
+var btn = document.getElementById("sembako");
+var span = document.getElementsByClassName("Modalfashion17close")[0];
+btn.onclick = function () {
+    modal17.style.display = "block";
+}
+span.onclick = function () {
+    modal17.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal17.style.display = "none";
+    }
+}
+
+var modal19 = document.getElementById("hobbys");
+var btn = document.getElementById("hobby");
+var span = document.getElementsByClassName("Modalfashion19close")[0];
+btn.onclick = function () {
+    modal19.style.display = "block";
+}
+span.onclick = function () {
+    modal19.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal19.style.display = "none";
+    }
+}
+
+
+var modal18 = document.getElementById("peralatans");
+var btn = document.getElementById("peralatan");
+var span = document.getElementsByClassName("Modalfashion18close")[0];
+btn.onclick = function () {
+    modal18.style.display = "block";
+}
+span.onclick = function () {
+    modal18.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal18.style.display = "none";
+    }
+}
